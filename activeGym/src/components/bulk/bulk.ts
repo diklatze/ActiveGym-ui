142cfd49637d152d4b3a19d80f48df071cfb004d
diklatze <diklatzemah@gmail.com>
2017-10-15 16:50:16 +0300

    bulk

--- activeGym/src/components/bulk/bulk.ts
+++ activeGym/src/components/bulk/bulk.ts
@@ -15,6 +15,7 @@ import { Http,Response,Headers } from '@angular/http';
   ]
 })
 export class Bulk implements OnInit{
+  submitResponse:number=0;
   bulkId:string;
   uploadDate:string;
   executionDate:string;
@@ -28,25 +29,27 @@ export class Bulk implements OnInit{
   constructor(private http: Http) {}
   
   bulkSubmit(){
+    this.submitResponse=1;
     let headers = new Headers;
     // headers.append('Content-Type', 'application/json');
     let body = {};
     this.http.post('http://service-request-for-payments.azurewebsites.net/bulk/'+this.bulkId+'/submit', JSON.stringify(body), { headers: headers })
-    .map(res => res.json())
-    .subscribe();
+    .map(res => {
+      // If request fails, throw an Error that will be caught
+      if(res.status < 200 || res.status >= 300) {
+        throw new Error('This request has failed ' + res.status);
+      } 
+      // If everything went fine, return the response
+      else {
+        
+        return res.json();
+      }
+    })
+    .subscribe(
+      
+    );
 
-    // this.http.post('http://service-request-for-payments.azurewebsites.net/bulk/'+this.bulkId+'/submit',body)
-
-    // this.http.get('https://service-payments.azurewebsites.net/bulks/search/is-new-requests?companyId=USACUGYMACTIVE&status=Uploaded')
-    // .map(res =>{ return res.json()})
-    // .subscribe(
-    //   data => { this.uploadDate = new Date(data._embedded.bulks[0].uploadDate).toLocaleDateString('en-US', this.options); 
-    //             this.bulkId = data._embedded.bulks[0].id;
-    //             this.executionDate = data._embedded.bulks[0].executionDate;
-    //             this.numberOfTransactions = data._embedded.bulks[0].numberOfTransactions.toString();
-    //             this.totalAmount = data._embedded.bulks[0].totalAmount.toLocaleString('en-US', {  style: 'currency',  currency: 'USD'});},
-     
-    //   );
+    
   }
 
   ngOnInit(): void {
