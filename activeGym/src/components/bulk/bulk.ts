bb20bfd6f01abdb16e894cab8062fdd8d33056ea
diklatze <diklatzemah@gmail.com>
2017-10-15 15:51:29 +0300

    http

--- activeGym/src/components/bulk/bulk.ts
+++ activeGym/src/components/bulk/bulk.ts
@@ -1,4 +1,11 @@
 import { Component } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import { OnInit } from '@angular/core';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/map';
+import { Http,Response,Headers } from '@angular/http';
+
+
 
 @Component({
   selector: 'bulk',
@@ -7,12 +14,58 @@ import { Component } from '@angular/core';
     ``,
   ]
 })
-export class Bulk {
+export class Bulk implements OnInit{
+  bulkId:string;
+  uploadDate:string;
+  executionDate:string;
+  numberOfTransactions:string;
+  totalAmount:string;
+  options = {
+    year: "numeric", month: "short",
+    day: "numeric"
+};
 
+  constructor(private http: Http) {}
+  
+  bulkSubmit(){
+    let headers = new Headers;
+    // headers.append('Content-Type', 'application/json');
+    let body = {};
+    this.http.post('http://service-request-for-payments.azurewebsites.net/bulk/'+this.bulkId+'/submit', JSON.stringify(body), { headers: headers })
+    .map(res => res.json())
+    .subscribe();
 
+    // this.http.post('http://service-request-for-payments.azurewebsites.net/bulk/'+this.bulkId+'/submit',body)
 
-  constructor() {
-   
+    // this.http.get('https://service-payments.azurewebsites.net/bulks/search/is-new-requests?companyId=USACUGYMACTIVE&status=Uploaded')
+    // .map(res =>{ return res.json()})
+    // .subscribe(
+    //   data => { this.uploadDate = new Date(data._embedded.bulks[0].uploadDate).toLocaleDateString('en-US', this.options); 
+    //             this.bulkId = data._embedded.bulks[0].id;
+    //             this.executionDate = data._embedded.bulks[0].executionDate;
+    //             this.numberOfTransactions = data._embedded.bulks[0].numberOfTransactions.toString();
+    //             this.totalAmount = data._embedded.bulks[0].totalAmount.toLocaleString('en-US', {  style: 'currency',  currency: 'USD'});},
+     
+    //   );
   }
 
+  ngOnInit(): void {
+    
+    
+    this.http.get('https://service-payments.azurewebsites.net/bulks/search/is-new-requests?companyId=USACUGYMACTIVE&status=Uploaded')
+    .map(res =>{ return res.json()})
+    .subscribe(
+      data => { this.uploadDate = new Date(data._embedded.bulks[0].uploadDate).toLocaleDateString('en-US', this.options); 
+                this.bulkId = data._embedded.bulks[0].id;
+                this.executionDate = data._embedded.bulks[0].executionDate;
+                this.numberOfTransactions = data._embedded.bulks[0].numberOfTransactions.toString();
+                this.totalAmount = data._embedded.bulks[0].totalAmount.toLocaleString('en-US', {  style: 'currency',  currency: 'USD'});},
+     
+      );
+  }
+   
+  
+  
 }
+
+
