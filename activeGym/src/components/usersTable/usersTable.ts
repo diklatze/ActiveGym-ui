171dba9490748e8638bc2959ba9d876b5fddc43e
diklatze <diklatzemah@gmail.com>
2017-10-16 17:40:32 +0300

    button

--- activeGym/src/components/usersTable/usersTable.ts
+++ activeGym/src/components/usersTable/usersTable.ts
@@ -1,16 +1,16 @@
-import { Component,Input } from '@angular/core';
-import { Http,Response,Headers } from '@angular/http';
-import {HelpService} from '../../services/help.services';
+import { Component, Input } from '@angular/core';
+import { Http, Response, Headers } from '@angular/http';
+import { HelpService } from '../../services/help.services';
 import { OnInit } from '@angular/core';
 
 class catagory {
   name: string;
   numOfTxs: number;
   totalAmount: number;
-  split : {
-      GOLD: number;
-      PLATINUM: number;
-      SILVER:number;
+  split: {
+    GOLD: number;
+    PLATINUM: number;
+    SILVER: number;
   }
 
 }
@@ -22,24 +22,38 @@ class catagory {
     ``,
   ]
 })
-export class UsersTable{
-  
-  @Input() bulkId:string;
-  @Input() numberOfTransactions:string;
-  @Input() totalAmount:string;
-  
+export class UsersTable {
+
+  @Input() bulkId: string;
+  @Input() numberOfTransactions: string;
+  @Input() totalAmount: string;
+
   categoriesStatus: catagory[];
-  constructor(private http: Http, public helpService:HelpService) {}
+  constructor(private http: Http, public helpService: HelpService) { }
+  check: number = 1;
 
-refresh (){
-  this.http.get('http://service-request-for-payments.azurewebsites.net/bulk/'+this.bulkId+'/status')
-  .map(res => { return res.json() })
-  .subscribe(
-  data => {
-    this.categoriesStatus = data.categoriesStatus;
+  refresh() {
+    this.check = 0;
+    this.http.get('http://service-request-for-payments.azurewebsites.net/bulk/' + this.bulkId + '/status')
+    .map(res => {
+      // If request fails, throw an Error that will be caught
+      if(res.status < 200 || res.status >= 300) {
+        this.check = 1;
+      } 
+      // If everything went fine, return the response
+      else {
+        
+        return res.json();
+      }
+    })
+      .subscribe(
+      data => {
+        this.categoriesStatus = data.categoriesStatus;
+        this.check = 1;
 
-  },
+      },
 
-);
-}
+    );
+   
+  }
 }
